17263d13ae9610c0b0d170f67543a9cdd191d690
sezgisyalcin <simreyalcin@gmail.com>
2025-06-23 03:41:46 +0300

    Update main.py

--- the_notes_that_are_very_useful_for_me_github/main.py
+++ the_notes_that_are_very_useful_for_me_github/main.py
@@ -1,1 +1,7 @@
 # Main Python file goes here. Replace with your working main.py
+git init
+git remote add origin https://github.com/KULLANICI_ADIN/smart-notes-app.git
+git add .
+git commit -m "initial commit"
+git branch -M main
+git push -u origin main
